948d6e3e0583841bd78689b2b43d3cca03ec5098
Pigeault <234028@tiho-hannover.de>
2026-08-01 14:35:03 +0200

    load helper function for AIScorrect_speed() and format package for CRAN

--- R/AIScorrect_speed.R
+++ R/AIScorrect_speed.R
@@ -82,9 +82,9 @@ AIScorrect_speed <- function(ais_data,
   doParallel::registerDoParallel(cl)
 
   corrected_data <- foreach::foreach(ais_data_core = ais_data,
-                          # .export = c("add_coordinates_meters"),
+                          .export = c("add_coordinates_meters", "AIStravel"),
                           .noexport = c("assign_mmsi_to_core", "ais_data"),
-                          .packages = c("dplyr", "sf", "AISanalyze")
+                          .packages = c("dplyr", "sf")
   ) %dopar% {
 
     ais_data <- ais_data_core %>%
